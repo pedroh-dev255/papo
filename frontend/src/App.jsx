7a0068a063053cd12e71c946822f4c7b10e7f152
Pedro Henrique <henriquepedro1912@gmail.com>
2026-08-03 16:46:48 -0300

    Converter sistema do tauri para eletron

--- frontend/src/App.jsx
+++
@@ -1,9 +0,0 @@
-﻿import AppRoutes from "./routes/AppRoutes";
-import NotificationOverlay from "./pages/NotificationOverlay";
-
-export default function App() {
-  const params = new URLSearchParams(window.location.search);
-  const overlay = params.get("overlay") === "true";
-
-  return overlay ? <NotificationOverlay /> : <AppRoutes />;
-}
