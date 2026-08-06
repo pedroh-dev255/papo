1801cf480da4b0eed1477b2ccc2d3f98e9da3c75
Pedro Henrique <henriquepedro1912@gmail.com>
2026-08-06 14:47:09 -0300

    correção no server/update.sh

--- servers/update.sh
+++ servers/update.sh
@@ -10,7 +10,6 @@ set +a
 
 envsubst < templates/livekit.yaml.template > generated/livekit.yaml
 envsubst < templates/turnserver.conf.template > generated/turnserver.conf
-envsubst < templates/nginx.conf.template > generated/nginx.conf
 
 docker compose down
 docker compose up -d
